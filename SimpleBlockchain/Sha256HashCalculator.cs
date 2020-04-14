8fba9a9c6ca2210f17caa1f464788fcff388b6e2
Kevin Gurton <kevin.gurton@gmail.com>
2020-04-14 16:16:40 +0100

    Added Sha256HashCalulcator

---
+++ SimpleBlockchain/Sha256HashCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace SimpleBlockchain
+{
+    public class Sha256HashCalculator : IHashCalculator
+    {
+        public IReadOnlyCollection<byte> CalculateHash(BlockCreationMetadata blockHeader, ITransaction transaction)
+        {
+            using var sha256 = SHA256.Create();
+            var metadataHash = blockHeader.GetHashBytes();
+            var transactionHash = transaction.GetHashBytes();
+            var allHashBytes = metadataHash.Concat(transactionHash);
+            return sha256.ComputeHash(allHashBytes.ToArray());
+        }
+    }
+}
