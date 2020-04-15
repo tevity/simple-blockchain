e9c2c88195cc91e4e9ef8fbc3b93c38c5b6245f5
Kevin Gurton <kevin.gurton@gmail.com>
2020-04-15 10:52:45 +0100

    Moved AddBLock method into interface

---
+++ SimpleBlockchain/BlockChain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleBlockchain
+{
+    public class BlockChain : IBlockChain
+    {
+        private readonly BlockBuilder _blockBuilder;
+        private readonly List<IBlock> _blocks;
+
+        public BlockChain(BlockBuilder blockBuilder, List<IBlock> blocks = null)
+        {
+            _blockBuilder = blockBuilder;
+            _blocks = blocks ?? new List<IBlock>();
+        }
+
+        public void AddBlock(IHashable transaction)
+        {
+            var previousBlock = _blocks.LastOrDefault();
+            var previousBlockHeader = previousBlock?.Header ?? BlockHeader.InitialisationHeader;
+            var block = _blockBuilder.BuildBlock(previousBlockHeader, transaction);
+            _blocks.Add(block);
+        }
+    }
+}
