e71d90fc3dae356e3c3be4bac8ee4b9fb534fd4d
Kevin Gurton <kevin.gurton@gmail.com>
2020-04-15 12:43:59 +0100

    Moved constructor to top of class

--- SimpleBlockchain/Block.cs
+++ SimpleBlockchain/Block.cs
@@ -2,13 +2,14 @@
 {
     public class Block : IBlock
     {
-        public BlockHeader Header { get; }
-        public IHashable Transaction { get; }
-
         public Block(BlockHeader header, IHashable transaction)
         {
             Header = header;
             Transaction = transaction;
         }
+
+        public BlockHeader Header { get; }
+
+        public IHashable Transaction { get; }
     }
 }
