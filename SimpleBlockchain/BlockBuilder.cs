8210dc223e3d2ec3ea1b3ddf462d72f92e54bda6
Kevin Gurton <kevin.gurton@gmail.com>
2020-04-15 16:16:36 +0100

    Refactored block creation

--- SimpleBlockchain/BlockBuilder.cs
+++ SimpleBlockchain/BlockBuilder.cs
@@ -19,11 +19,7 @@ namespace SimpleBlockchain
                 previousBlock.BlockNumber + 1,
                 _dateTimeProvider.Now,
                 previousBlock.Hash);
-            var hash = _hashCalculator.CalculateHash(creationMetadata, transaction);
-            var header = new BlockHeader(creationMetadata, hash);
-            var newBlock = new Block(header, transaction);
-            previousBlock.NextBlock = newBlock;
-            return newBlock;
+            return CreateBlock(transaction, creationMetadata);
         }
 
         public IBlock BuildGenesisBlock<TTransaction>(TTransaction transaction) where TTransaction : IHashable
@@ -32,6 +28,12 @@ namespace SimpleBlockchain
                 0,
                 _dateTimeProvider.Now,
                 Enumerable.Empty<byte>().ToArray());
+            return CreateBlock(transaction, creationMetadata);
+        }
+
+        private IBlock CreateBlock<TTransaction>(TTransaction transaction, HashableBlockHeader creationMetadata)
+            where TTransaction : IHashable
+        {
             var hash = _hashCalculator.CalculateHash(creationMetadata, transaction);
             var header = new BlockHeader(creationMetadata, hash);
             var newBlock = new Block(header, transaction);
